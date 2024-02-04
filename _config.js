8e604fa5c93141205ce51bd8eec4e4623b7781e1
Carl Räfting <mail@carlrafting.com>
2024-02-04 12:33:57 +0100

    remove nunjucks plugin

--- _config.js
+++ _config.js
@@ -1,12 +1,10 @@
 import lume from "lume/mod.ts";
-import nunjucks from "lume/plugins/nunjucks.ts";
 import feed from "lume/plugins/feed.ts"
 
 const site = lume({
     src: './src'
 });
 site.use(feed());
-site.use(nunjucks());
 
 site.copy(['.css', '.js']);
 
