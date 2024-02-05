6a110889aed6f3f29c1ef1fbf077306dd3ba6d2d
Carl Räfting <mail@carlrafting.com>
2024-02-05 21:36:36 +0100

    update lume configuration

--- _config.js
+++ _config.js
@@ -2,10 +2,20 @@ import lume from "lume/mod.ts";
 import feed from "lume/plugins/feed.ts"
 
 const site = lume({
+    location: new URL('https://carlrafting.com'),
     src: './src'
 });
 site.use(feed());
 
+site.filter('log', (value) => console.log(value));
+site.filter('date', (value) => {
+    const locale = new Intl.DateTimeFormat('sv-se');
+    return locale.format(value);
+});
+
 site.copy(['.css', '.js']);
+site.copyRemainingFiles();
+
+// console.log({ site });
 
 export default site;
