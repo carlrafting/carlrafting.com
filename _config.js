b56e67289911cd78900d8ec53479711cb3ece32e
Carl Räfting <mail@carlrafting.com>
2024-02-07 22:38:59 +0100

    enable code highlighting

--- _config.js
+++ _config.js
@@ -1,10 +1,24 @@
 import lume from "lume/mod.ts";
-import feed from "lume/plugins/feed.ts"
+import codeHighlight from "lume/plugins/code_highlight.ts";
+import feed from "lume/plugins/feed.ts";
+
+import lang_xml from "npm:highlight.js/lib/languages/xml";
+import lang_css from "npm:highlight.js/lib/languages/css";
+import lang_javascript from "npm:highlight.js/lib/languages/javascript";
+import lang_bash from "npm:highlight.js/lib/languages/bash";
 
 const site = lume({
     location: new URL('https://carlrafting.com'),
     src: './src'
 });
+site.use(codeHighlight({
+  languages: {
+    xml: lang_xml,
+    css: lang_css,
+    javascript: lang_javascript,
+    bash: lang_bash,
+  },
+}));
 site.use(feed());
 
 site.filter('log', (value) => console.log(value));
