a7c35ecb6bdc5192e7af2ea5f4a5b97ea7697055
Carl Räfting <121248+carlrafting@users.noreply.github.com>
2024-04-03 20:17:40 +0200

    update lume cms config

--- _cms.ts
+++ _cms.ts
@@ -1,13 +1,30 @@
-import lumeCMS from "lume/cms.ts";
+import lumeCMS, { Kv } from "lume/cms.ts";
 
 const cms = lumeCMS();
+const kv = await Deno.openKv();
 
-cms.storage("storage", "storage");
+cms.storage("bookmarks", new Kv({ kv }));
 
-cms.collection("posts", "src:posts/*.md", [
+cms.collection("posts: Blog posts on various topics...", "src:posts/*.md", [
   "title: text",
   "description: text",
   "slug: text",
+  "content: markdown",
+]);
+
+cms.collection(
+  "bookmarks: Bookmarks from around the world wide web i find interesting",
+  "bookmarks",
+  [
+    "title: text",
+    "url: url",
+    "description: markdown",
+    "datetime: datetime",
+  ],
+);
+
+cms.document("about: About the website and me...", "src:about.md", [
+  "title: text",
   "content: markdown",
 ]);
 
