fe44940a6d63666180a31b8662be46bdb70b8743
Carl Räfting <121248+carlrafting@users.noreply.github.com>
2024-06-10 21:50:31 +0200

    update posts data

--- src/posts/_data.js
+++ src/posts/_data.js
@@ -3,8 +3,6 @@
  * @returns {string|undefined} slug url if it exists
  */
 export const url = (page) => {
-  const { slug } = page.data;
-  if (slug) {
-    return `/post/${slug}/`;
-  }
+  const { basename, slug } = page.data;
+  return `/post/${slug ? slug : basename}/`;
 };
