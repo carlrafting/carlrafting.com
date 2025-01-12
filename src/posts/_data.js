1531e155a8debb70e41669f0971e0c4da37f4a27
Carl Räfting <121248+carlrafting@users.noreply.github.com>
2025-01-12 14:08:58 +0100

    add date to post permalinks

--- src/posts/_data.js
+++ src/posts/_data.js
@@ -1,9 +1,11 @@
+import { format } from "@std/datetime/format";
+
 /**
  * @param {Lume.Page} page
  * @returns {string|undefined} slug url if it exists
  */
 export const url = (page) => {
-  const { basename, slug } = page.data;
-  // return `/post/${slug ? slug : basename}/`;
-  return `/${slug ? slug : basename}/`;
+  const { basename, slug, date } = page.data;
+  const urlDate = format(date, "yyyy/MM/dd");
+  return `/${urlDate}/${slug ? slug : basename}/`;
 };
